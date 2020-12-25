ff010f955c9ec8e05d23a2871609ad5b7b1d5900
Коля Васильев <nikolayvas20@gmail.com>
2020-12-25 14:08:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            while (str.Contains("  "))
+            { 
+                str = str.Replace("  ", " ");
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
